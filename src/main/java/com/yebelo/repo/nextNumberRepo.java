6feccecf71a313033bed56d73bd46099868d95f6
Ankit Kumar Sinha <ankitsinha111200@gmail.com>
2023-06-21 04:13:06 +0530

    initialize database configuration with hibernate and debug some issues

--- src/main/java/com/yebelo/repo/nextNumberRepo.java
+++ src/main/java/com/yebelo/repo/nextNumberRepo.java
@@ -2,10 +2,16 @@ package com.yebelo.repo;
 
 import com.yebelo.entity.nextNumberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface nextNumberRepo extends JpaRepository<nextNumberEntity, Long> {
     nextNumberEntity findByCategoryCode(String categoryCode);
-    void updateValue(int newValue);
+
+    @Modifying
+    @Query("UPDATE nextNumberEntity n SET n.value = :value")
+    void updateValue(@Param("value") int value);
 }
